4bfd4fb1ebad860c2d433e33967b51eecf2f060b
carlosgonzalez96 <carlosgonz817@gmail.com>
2019-04-11 11:04:05 -0700

    Added test and added new features status and departments

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,10 +1,12 @@
 class Person
-	attr_accessor :full_name, :dob, :job_title
+	attr_accessor :full_name, :dob, :job_title, :department, :status
 
-	def initialize(full_name, dob, job_title)
+	def initialize(full_name, dob, job_title, department, status)
 		@full_name = full_name
 		@dob = dob
 		@job_title = job_title
+		@department = department
+		@status = status
 	end
 
 	def in_view?
--- spec/models/popular_spec.rb
+++ spec/models/popular_spec.rb
@@ -1,13 +1,14 @@
 require "rails_helper"
 
 RSpec.describe Person do
-	let (:person) { Person.new("Frank Reynolds", "01/31/1943", "financer") }
-	let (:next_person) { Person.new("Charlie Day", "02/09/1976", "janitor") }
+	let (:person) { Person.new("Frank Reynolds", "01/31/1943", "financer", "Finance", "NA") }
+	let (:next_person) { Person.new("Charlie Day", "02/09/1976", "janitor", "Maintenance", "A") }
 	
 	it "has distinctive information" do
 		expect(person.full_name) == "Frank Reynolds"
 		expect(person.dob) == "01/31/1943"
 		expect(person.job_title) == "financer"
+		expect(person.department) == "Finance"
 	end
 
 	it "can be the person being viewed" do
@@ -21,6 +22,17 @@ RSpec.describe Person do
 		expect(person.on_deck?) == false
 		expect(next_person.in_view?) == false
 		expect(next_person.on_deck?) == true
+	end
+
+	it "has to have information" do
+		expect(person.full_name).to be_truthy
+		expect(person.dob).to be_truthy
+		expect(person.job_title).to be_truthy
+	end
+
+	it "needs to be current staff" do
+		expect(person.status) == "NA"
+		expect(next_person.status) == "A"
 	end
 
 end
