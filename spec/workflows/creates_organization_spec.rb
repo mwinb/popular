bf5928111419f025d40ca095dad1664e940a92c1
carlosgonzalez96 <carlosgonz817@gmail.com>
2019-05-06 09:32:20 -0700

    added tests in creates organization spec

---
+++ spec/workflows/creates_organization_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+RSpec.describe CreatesOrganization do
+    describe "initialization" do
+        it "creates organization given name" do
+            creator = CreatesOrganization.new(organization_name: "Paddys Pub")
+            creator.build
+            expect(creator.organization.name).to eq("Paddys Pub")
+        end
+
+        it "creates organization given name and location" do
+            creator = CreatesOrganization.new(organization_name: "Paddys Pub", organization_location: "PA")
+            creator.build
+            expect(creator.organization.name).to eq("Paddys Pub")
+            expect(creator.organization.location).to eq("PA")
+        end
+    end
+
+    describe "error case" do
+        it "checks if a entries are empty" do
+            creator = CreatesOrganization.new(organization_name: "", organization_location: nil)
+            creator.create
+            #expect(creator).not_to be_a_success
+            expect(creator.organization.name).to eq("")
+            expect(creator.organization.location).to eq(nil)
+        end
+
+        it "checks if an entry is empty" do
+            creator = CreatesOrganization.new(organization_name: "Paddys Pub", organization_location: nil)
+            creator.create
+            assert_equal creator.organization.location, nil, "there is some kind of location still"
+        end
+
+        it "checks if an organization name is filled" do
+            creator = CreatesOrganization.new(organization_name: "Paddys Pub", organization_location: "PA")
+            creator.create
+            assert_equal creator.organization.name, "Paddys Pub", "The entry is nil"
+            #assert_equal creator.organization.name, nil, "The entry is nil"
+        end
+
+        it "checks if a location is selected" do
+            creator = CreatesOrganization.new(organization_name: "Paddys Pub", organization_location: "PA")
+            creator.create
+            assert_nil creator.organization.location.nil?
+        end
+
+    end
+end
