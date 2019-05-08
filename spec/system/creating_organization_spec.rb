586b93ec22abab258233fb76b0cb6e934a8d538f
teemal <taymal1987@gmail.com>
2019-05-08 09:08:17 -0700

    Organization: Pass org creation system test. Fail sad path.
    Need to add rules at the model layer.

---
+++ spec/system/creating_organization_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe "creating an organization", type: :system do
+
+    it "allows a user to create an organization" do
+        fake_name = "FAKE ORGANIZATION"
+        fake_location = "FAKE LOCATION"
+
+        visit new_organization_path
+        fill_in "Name", with: fake_name
+        fill_in "Location", with: fake_location
+        click_on("Create Organization")
+
+        expect(page).to have_content(fake_name)
+        expect(page).to have_content(fake_location)
+    end
+
+    it "does not allow a user to create an organization without a name" do
+        visit new_organization_path
+        fill_in "Name", with: ""
+        fill_in "Location", with: "FAKE LOCATION"
+        click_on("Create Organization")
+        expect(page).to have_content("Error")
+    end
+
+end
