c778456da08d8b7d4334c7e65d82864d89a9215e
teemal <taymal1987@gmail.com>
2019-05-08 08:44:45 -0700

    Models: Remove old cruft, fix association name and test.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,17 +1,5 @@
 class Organization < ApplicationRecord
-	# attr_accessor :name, :location, :departments, :employees
-	has_many :employee
-	# @employees = []
-	# def initialize(name, location)
-	# 	@name = name
-	# 	@location = location
-	# 	@employees = []
-	# end
 
-	def add_employee(employee)
-		if employee.organization == @name
-			employees << employee.full_name
-		end
-	end
+	has_many :employees
 
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,16 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Organization do
-    let (:org) {Organization.new(name:"Paddys Pub", location:"PA")}
+    let (:organization) { Organization.new(name:"Paddys Pub", location:"PA") }
     let (:employee) { Employee.new(full_name:"Dennis Reynolds", dob:"08-18-1976", job_title:"Golden God", organization: Organization.new(name:"Paddys Pub", location:"PA"), department:"Management", status:"NA") }
-    let (:next_employee) { Employee.new(full_name:"Dee Reynolds", dob:"08-18-1976", job_title:"Bird", organization: Organization.new(name:"Paddys Pub", location:"PA"), department:"food and beverage services", status:"A") }
-    let (:rickity_cricket) {Employee.new(full_name:"Mathew Mara", dob:"01-01-1976", job_title:"Street Rat", organization:Organization.new(name:"Some Street", location:"PA"), department:"Some Alley", status:"NA") }
-    
-    it "adds employees to itself" do
-        # byebug
-        org.add_employee(employee)
-        org.add_employee(next_employee)
-        # byebug
+
+    it "has employees" do
+        expect(organization).to respond_to(:employees)
     end
 
     it "only adds employees to itself if they belong to organization" do
