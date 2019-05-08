a45b73815ba0ae97cc6620738fca3ebb146ddd8a
teemal <taymal1987@gmail.com>
2019-05-08 08:53:19 -0700

    Employee: Test organization presence and add pending stuff for Taylor and Carlos to do.

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:employee) { Employee.new }
+
+  it "belongs to an organization" do
+    expect(employee).to respond_to(:organization)
+  end
+
+  pending "has a full_name"
+  pending "has a etc..."
+
 end
