06f588f0ae2e39983e87ac470e53889e92abc9ec
teemal <taymal1987@gmail.com>
2019-05-08 17:37:06 -0700

    Organization: change location test to check if nil when no location given

--- spec/workflows/creates_organization_spec.rb
+++ spec/workflows/creates_organization_spec.rb
@@ -39,9 +39,9 @@ RSpec.describe CreatesOrganization do
         end
 
         it "checks if a location is selected" do
-            creator = CreatesOrganization.new(organization_name: "Paddys Pub", organization_location: "PA")
+            creator = CreatesOrganization.new(organization_name: "Paddys Pub")
             creator.create
-            assert_nil creator.organization.location.nil?
+            expect(creator.organization.location).to be_nil
         end
 
     end
