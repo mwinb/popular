56371a95cbdeb0917b8d8ebbb1dbbbd6e8ba7f85
teemal <taymal1987@gmail.com>
2019-05-11 14:57:47 -0700

    Employee: stub out unit tests in model
    Tests pass but need some more meat to them. Currently only validating 2 employee attributes

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,6 @@ class Employee < ApplicationRecord
 	belongs_to :organization
 
 	#validates :dob, presence: true
+	validates :full_name, presence: true
+	validates :organization, presence: true
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -8,7 +8,37 @@ RSpec.describe Employee, type: :model do
     expect(employee).to respond_to(:organization)
   end
 
-  pending "has a full_name"
-  pending "has a etc..."
+  it "has a full name" do
+    expect(employee).to respond_to(:full_name)
+  end
+
+  it "has a job title" do
+   expect(employee).to respond_to(:job_title)
+  end
+
+  it "has a dob" do
+    expect(employee).to respond_to(:dob)
+   end
+
+   it "belongs to an organization" do
+    employee.organization = Organization.new(name: "FAKE ORGANIZATION", location: "fake place")
+    expect(employee.organization.name).to eq("FAKE ORGANIZATION") 
+   end
+
+  it "is invalid without a name" do
+    employee.organization = Organization.new(name: "FAKE ORGANIZATION", location: "fake place")
+    employee.full_name = "Fake Name"
+    expect(employee).to be_valid
+    employee.full_name = ""
+    expect(employee).not_to be_valid
+  end
+
+  it "is invalid without an Organization" do
+    employee.organization = Organization.new(name: "FAKE ORGANIZATION", location: "fake place")
+    employee.full_name = "Fake Name"
+    expect(employee).to be_valid
+    employee.organization = nil
+    expect(employee).not_to be_valid
+  end
 
 end
