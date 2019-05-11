1d058d3b2329fac4ec7c126c22e473179aceeb8a
teemal <taymal1987@gmail.com>
2019-05-11 15:33:06 -0700

    Employee schema: change DOB attribute from date object to string

--- db/migrate/20190425154049_create_employees.rb
+++ db/migrate/20190425154049_create_employees.rb
@@ -2,7 +2,7 @@ class CreateEmployees < ActiveRecord::Migration[5.2]
   def change
     create_table :employees do |t|
       t.string :full_name
-      t.date :dob
+      t.string :dob
       t.string :job_title
       t.string :department
       t.string :status
