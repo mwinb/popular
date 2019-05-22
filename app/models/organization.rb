d96696e7aab6695a2ef263bb2d976aef7831da89
mwinb <mwinb>
2019-05-22 11:38:04 -0700

    Remove big dependency from organization model and organization model tests

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,9 +5,4 @@ class Organization < ApplicationRecord
 	validates :name, presence: true 
 	validates :location, presence: true
 
-	def perform(big_dependency)
-		big_dependency.execute
-		return 42
-	end
-
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,9 +2,6 @@ require "rails_helper"
 
 RSpec.describe Organization do
     let (:organization) { FactoryBot.build_stubbed(:organization) }
-    before(:each) do
-        @mock_dependency = double("some_dependency")
-    end
 
     it "has employees" do
         expect(organization).to respond_to(:employees)
@@ -30,14 +27,4 @@ RSpec.describe Organization do
         expect(organization).not_to be_valid
     end
     
-    it "(fake) returns a result from an organization performing" do
-        result = organization.perform(LittleDependency.new)
-        expect(result).to eq(42)
-    end
-
-    it "(stub/mock) returns a result from an organization performing" do
-        allow(@mock_dependency).to receive(:execute)
-        result = organization.perform(@mock_dependency)
-        expect(result).to eq(42)
-    end
 end
