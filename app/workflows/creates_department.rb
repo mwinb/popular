5caaa62be4ae7e13a5f73bc646bc94f35976bbdd
Myles Moylan <moylanm@oregonstate.edu>
2019-06-10 23:47:03 -0700

    fix department to take in organization id

--- app/workflows/creates_department.rb
+++ app/workflows/creates_department.rb
@@ -1,11 +1,10 @@
 class CreateDepartment
   attr_accessor :department_name, :organization, :employees
 
-  def initialize(department_name: "",
-                 organization: "",
-                 employees: "")
+  def initialize(department_name: nil,
+                 organization_id: "")
     @department_name = department_name
-    @organization = organization
+    @organization_id = organization_id
   end
 
   def build
@@ -22,4 +21,9 @@ class CreateDepartment
   def success?
     @success
   end
+
+  def get_organization_by_id(organization_id)
+    Organization.find(organization_id) if organization_id != ""
+  end
+
 end
